4de605836cc0709356673ec3333cfdaad49c36b4
SiBreaker <hayk.ge@gmail.com>
2016-10-10 09:54:01 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ft(5));
+        }
+
+        static int ft(int a)
+        {
+            if (a == 1)
+                return 1;
+            return ft(a - 1) * a;
+        }
+
+
+    }
+}
